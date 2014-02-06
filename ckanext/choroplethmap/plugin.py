f9c197ded526c23a13ec98ddf5c60e3d095b5418
Vitor Baptista <vitor@vitorbaptista.com>
2014-02-06 17:57:55 -0300

    Adds input for GeoJSON URL in the form

--- ckanext/choroplethmap/plugin.py
+++ ckanext/choroplethmap/plugin.py
@@ -1,5 +1,7 @@
 import ckan.plugins as p
 
+not_empty = p.toolkit.get_validator('not_empty')
+
 
 class ChoroplethMap(p.SingletonPlugin):
     '''Creates a choropleth map view'''
@@ -12,8 +14,28 @@ class ChoroplethMap(p.SingletonPlugin):
         p.toolkit.add_resource('theme/public', 'choroplethmap')
 
     def info(self):
-        return {'icon': 'map-marker',
+        schema = {
+            'geojson_url': [not_empty]
+        }
+
+        return {'name': 'choropleth-map',
+                'title': 'Choropleth Map',
+                'icon': 'map-marker',
+                'schema': schema,
                 'iframed': False}
 
     def can_view(self, data_dict):
         return data_dict['resource'].get('datastore_active', False)
+
+    def setup_template_variables(self, context, data_dict):
+        resource = data_dict['resource']
+        resource_view = data_dict['resource_view']
+
+        return {'resource': resource,
+                'resource_view': resource_view}
+
+    def view_template(self, context, data_dict):
+        return 'choroplethmap_view.html'
+
+    def form_template(self, context, data_dict):
+        return 'choroplethmap_form.html'
--- ckanext/choroplethmap/tests/test_view.py
+++ ckanext/choroplethmap/tests/test_view.py
@@ -34,3 +34,52 @@ class TestChoroplethMap(object):
             'resource': { 'datastore_active': False }
         }
         assert not self.plugin.can_view(inactive_datastore_data_dict)
+
+    def test_schema_exists(self):
+        schema = self.plugin.info()['schema']
+        assert schema is not None, 'Plugin should define schema'
+
+    def test_schema_has_geojson_url(self):
+        schema = self.plugin.info()['schema']
+        assert schema.get('geojson_url') is not None, \
+            'Schema should define "geojson_url"'
+
+    def test_schema_geojson_url_isnt_empty(self):
+        schema = self.plugin.info()['schema']
+        not_empty = p.toolkit.get_validator('not_empty')
+        assert not_empty in schema['geojson_url'], \
+            '"geojson_url" should not be empty'
+
+    def test_plugin_isnt_iframed(self):
+        iframed = self.plugin.info().get('iframed', True)
+        assert not iframed, 'Plugin should not be iframed'
+
+    def test_setup_template_variables_adds_resource(self):
+        resource = {
+            'id': 'resource_id',
+        }
+
+        template_variables = self._setup_template_variables(resource)
+
+        assert 'resource' in template_variables
+        assert template_variables['resource'] == resource
+
+    def test_setup_template_variables_adds_resource_view(self):
+        resource_view = {
+            'id': 'resource_id',
+            'other_attribute': 'value'
+        }
+
+        template_variables = \
+            self._setup_template_variables(resource_view=resource_view)
+
+        assert 'resource_view' in template_variables
+        assert template_variables['resource_view'] == resource_view
+
+    def _setup_template_variables(self, resource={}, resource_view={}):
+        context = {}
+        data_dict = {
+            'resource': resource,
+            'resource_view': resource_view
+        }
+        return self.plugin.setup_template_variables(context, data_dict)
