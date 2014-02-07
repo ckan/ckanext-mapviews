c996afdb3b9d5792de5986284cb61f16d3e1210d
Vitor Baptista <vitor@vitorbaptista.com>
2014-02-07 16:02:52 -0300

    Fit map to geojson layer

--- ckanext/choroplethmap/theme/public/choroplethmap.js
+++ ckanext/choroplethmap/theme/public/choroplethmap.js
@@ -5,27 +5,32 @@ ckan.module("choroplethmap", function (jQuery) {
     var id = this.el.attr('id'),
         geojsonUrl = this.options.geojsonUrl,
         geojsonProperty = this.options.geojsonProperty,
-        map = L.map(id).setView([30.4, 69.3], 4);
+        map = L.map(id);
 
-    _addBaseLayer(map);
-    _addGeoJSONLayer(map, geojsonUrl, geojsonProperty);
+    jQuery.getJSON(geojsonUrl, function (geojson) {
+      var geojsonLayer;
+      _addBaseLayer(map);
+      geojsonLayer = _addGeoJSONLayer(map, geojson, geojsonProperty);
+      map.fitBounds(geojsonLayer.getBounds());
+    });
   }
 
   function _addBaseLayer(map) {
-    L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
+    return L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
       key: 'd4fc77ea4a63471cab2423e66626cbb6',
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
       maxZoom: 18
     }).addTo(map);
   }
 
-  function _addGeoJSONLayer(map, geojsonUrl, geojsonProperty) {
-    jQuery.getJSON(geojsonUrl, function (geojson) {
+  function _addGeoJSONLayer(map, geojson, geojsonProperty) {
       var scale = _createScale(geojson, geojsonProperty);
 
-      L.geoJson(geojson, { style: _geoJsonStyle(scale, geojsonProperty) }).addTo(map);
       _addLegend(map, scale);
-    });
+
+      return L.geoJson(geojson, {
+        style: _geoJsonStyle(scale, geojsonProperty)
+      }).addTo(map);
   }
 
   function _geoJsonStyle(scale, geojsonProperty) {
